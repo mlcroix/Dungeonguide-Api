62ae49a0e257f502cd63ad9c0d8a037dd7b41fae
Michel du Croix <mlcroix@avans.nl>
2018-05-27 21:53:54 +0200

    cor fix 2.0

--- app.js
+++ app.js
@@ -33,9 +33,14 @@ app.use('/notes', notesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+//cor fix
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next(createError(404));
+  next();
 });
 
 // error handler
