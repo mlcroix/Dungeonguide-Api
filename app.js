617a46b23609023af2dcc67dc4bc451c3334d87f
Michel du Croix <mlcroix@avans.nl>
2018-10-27 12:22:20 +0200

    cleaning up 2.0
    removing unused packages,
    updating packages
    removng code for views which I don't use
    adding websocket support

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var MongoClient = require('mongodb').MongoClient;
+var db = require('./db');
+
+db.connect();
 
 var indexRouter = require('./routes/index');
 var playersRouter = require('./routes/players');
 var campaignsRouter = require('./routes/campaigns');
 var sessionsRouter = require('./routes/sessions');
 var notesRouter = require('./routes/notes');
-var app = express();
-var MongoClient = require('mongodb').MongoClient;
-var db = require('./db');
-
-db.connect();
-
 
 // Add headers
 app.use(function (req, res, next) {
@@ -35,38 +31,23 @@ app.use(function (req, res, next) {
   next();
 });
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
 app.use('/players', playersRouter);
 app.use('/campaigns', campaignsRouter);
 app.use('/sessions', sessionsRouter);
 app.use('/notes', notesRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+/*
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
-
-
-
-module.exports = app;
+*/
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
